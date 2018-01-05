92b439f9e094f1bd3805c187dcec7d5b641ee46b
Andrii Pelekh <andrii.pelekh@nasdaq.com>
2018-01-05 16:16:24 -0700

    Initial commit

---
+++ src/main/java/com/andriiP/reactbookpractice/ReactBookPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.andriiP.reactbookpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBookPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBookPracticeApplication.class, args);
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require("webpack")
+var path = require("path")
+
+process.noDeprecation = true
+
+module.exports = {
+  entry: "./src/main/resources/static/js/index.js",
+  output: {
+      path: "./src/main/resources/static/dist",
+      filename: "bundle.js",
+      sourceMapFilename: 'bundle.map'
+  },
+  devtool: '#source-map',
+  module: {
+      rules: [
+          {
+              test: /\.js$/,
+              exclude: /(node_modules)/,
+              loader: 'babel-loader',
+              query: {
+                  presets: ['env', 'stage-0', 'react']
+              }
+          },
+          {
+              test: /\.css$/,
+              use: ['style-loader','css-loader', {
+                  loader: 'postcss-loader',
+                  options: {
+                    plugins: () => [require('autoprefixer')]
+                  }}]
+          }
+      ]
+  },
+  plugins: [
+
+      new webpack.DefinePlugin({
+          "process.env": {
+              NODE_ENV: JSON.stringify("production")
+          }
+      }),
+
+      new webpack.optimize.UglifyJsPlugin({
+          sourceMap: true,
+          warnings: false,
+          mangle: false
+      })
+  ]
+}
